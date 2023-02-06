b6304c08d7d3bc906d82d5af1cf304038e2728dd
Jayanth-Parthsarathy <amjayanthparthsarathy.cse2021@citchennai.net>
2023-02-06 21:00:17 +0530

    add navbar and styling

---
+++ travel-journal/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <img className="nav--img" src="images/earth.png" alt="earth" />
+      <h6 className="nav--text">my travel journal.</h6>
+    </div>
+  );
+}
